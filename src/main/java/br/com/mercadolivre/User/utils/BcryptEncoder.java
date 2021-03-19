f9e57259f8884ee3637644be38923bb3bb335d32
Afonso Mateus <afonsoprad10@gmail.com>
2021-03-19 16:16:28 -0400

    hashing password if plain text

---
+++ src/main/java/br/com/mercadolivre/User/utils/BcryptEncoder.java
@@ -0,0 +1,18 @@
+package br.com.mercadolivre.User.utils;
+
+import java.util.regex.Pattern;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class BcryptEncoder {
+	private static Pattern BCRYPT_PATTERN =
+			Pattern.compile("\\A\\$2a?\\$\\d\\d\\$[./0-9A-Za-z]{53}");
+
+	public static String encodeIfPlainText(String password) {
+		var matcher = BCRYPT_PATTERN.matcher(password);
+		if (matcher.matches()) {
+			return password;
+		}
+		var encoder = new BCryptPasswordEncoder();
+		return encoder.encode(password);
+	}
+}
