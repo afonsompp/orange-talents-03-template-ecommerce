359c8392f6b8a876e93a132d9eb78671a6016240
Afonso Mateus <afonsoprad10@gmail.com>
2021-03-21 05:23:15 -0400

    test parse method of CategoryRequest

---
+++ src/test/java/br/com/mercadolivre/product/CategoryRequestTest.java
@@ -0,0 +1,45 @@
+package br.com.mercadolivre.product;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.Optional;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import br.com.mercadolivre.Product.dto.CategoryRequest;
+import br.com.mercadolivre.Product.model.Category;
+import br.com.mercadolivre.Product.repository.CategoryRepository;
+
+@SpringBootTest
+public class CategoryRequestTest {
+
+	@MockBean
+	CategoryRepository repository;
+
+	@Test
+	@DisplayName("Should return category with caregory relationship null")
+	public void test1() {
+		var request = new CategoryRequest("name", null);
+
+		var category = request.parseToCategory(repository);
+
+		assertEquals(null, category.getCategory());
+
+	}
+
+	@Test
+	@DisplayName("Should return category with caregory relationship null")
+	public void test2() {
+
+		Category cat = new Category("name");
+		Mockito.when(repository.findById(1L)).thenReturn(Optional.of(cat));
+
+		var request = new CategoryRequest("name2", 1L);
+		var result = request.parseToCategory(repository);
+
+		assertEquals(cat.getName(), result.getCategory().getName());
+
+	}
+
+}
