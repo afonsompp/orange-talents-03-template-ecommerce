ad593b48b644945c11d1912e18d238be025e9175
Afonso Mateus <afonsoprad10@gmail.com>
2021-03-20 22:17:30 -0400

    Add validation in UserRequest

--- src/main/java/br/com/mercadolivre/User/dto/UserRequest.java
+++ src/main/java/br/com/mercadolivre/User/dto/UserRequest.java
@@ -5,14 +5,21 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import br.com.mercadolivre.User.model.User;
+import br.com.mercadolivre.Validation.UniqueFieldConstraint;
 
 public class UserRequest {
 	@NotBlank
 	@Email
+	@UniqueFieldConstraint(entityClass = User.class, field = "login")
 	private String login;
 	@NotBlank
 	@Size(min = 6)
 	private String password;
+
+	public UserRequest(String login, String password) {
+		this.login = login;
+		this.password = password;
+	}
 
 	@Deprecated
 	public UserRequest() {
