59caf2266134256ab6e7a1ef02129a978642c4b4
Afonso Mateus <afonsoprad10@gmail.com>
2021-03-23 08:45:33 -0400

    add access to static resources

--- src/main/java/br/com/mercadolivre/security/configuration/SecurityConfiguration.java
+++ src/main/java/br/com/mercadolivre/security/configuration/SecurityConfiguration.java
@@ -13,6 +13,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -64,6 +65,13 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(this.usersService)
 				.passwordEncoder(new BCryptPasswordEncoder());
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/**.html", "/v2/api-docs", "/webjars/**",
+				"/configuration/**",
+				"/swagger-resources/**", "/h2-console/**", "/favicon.ico/**");
 	}
 
 	private static class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
